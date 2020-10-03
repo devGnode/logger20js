7bf6f215a389a853d0a28449042d0874e6dd495b
maroder <looterofflux@gmail.com>
2020-10-03 22:13:27 +0200

    Realase 1.2.0

--- export/Logger20js.js
+++ export/Logger20js.js
@@ -1,6 +1,6 @@
 const {Logger} = require("../src/Logger");
 const {Color} = require("../src/Colorize");
-const {Loader} = require("../src/loader");
+const {Loader} = require("../src/Loader");
 
 function g( r = void 0||undefined ){}
 
