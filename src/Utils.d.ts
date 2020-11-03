2aae8807a19685447e11a92b311fffe914b41099
maroder <no-reply@provider.tld>
2020-11-03 12:08:07 +0100

    remove all mounted files

--- src/Utils.d.ts
+++
@@ -1,6 +0,0 @@
-export declare class Utils {
-    static round(value?: any): any;
-    static merge(objA?: Object, ...args: Object[]): Object;
-    static parseTime(timeStamp?: number): string;
-    static getRotateTimestampOutOf(rotate?: string, date?: Date): Date;
-}
